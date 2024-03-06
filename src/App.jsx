912882e1d9f705044ced00affea0faec656faec1
Anezijane <ahnnieanezi@gmail.com>
2024-03-06 22:25:11 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <Navbar /> 
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return(
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <h1>LOOPSTUDIOS</h1>
+                
+                    <li>About</li>
+                    <li>Careers</li>
+                    <li>Products</li>
+                    <li>Support</li>
+                
+            </nav>
+        </>
+    )
+}
