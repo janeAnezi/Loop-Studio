c101abd04b24f9499b7417e9ce43d6837d200db2
Anezijane <ahnnieanezi@gmail.com>
2024-03-08 23:42:31 +0100

    add backgroud image to header section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar"
 export default function Home() {
     return(
         <>
-            <div>
+            <div className="bg-hero-pattern bg-no-repeat bg-cover h-screen">
                 <Navbar />
                 <h1>IMMERSIVE EXPERIENCE THAT DELIVER</h1>
             </div>
