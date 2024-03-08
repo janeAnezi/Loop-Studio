baa30a0e0bdcc1cd3b9e692fa1b25c8fe4cd30ca
Anezijane <ahnnieanezi@gmail.com>
2024-03-08 23:17:50 +0100

    install tailwind

---
+++ src/Components/Careeres.jsx
@@ -0,0 +1,19 @@
+export default function Careers() {
+    return(
+        <>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <h2>THE HEADER IN INTERACTIVE VR</h2>
+                    <p>Creates highly immersive environments that surround users, 
+                        making them feel as though they are physically present within
+                         the digital space. This immersive experience is achieved through
+                          the use of VR headsets, which typically cover the user's field of 
+                          view and block out the real-world environment.</p>
+                </div>
+            </div>
+        </>
+    )
+}
