66205efc3389a789efe4c6ce3e66d0170ef51cbb
Anezijane <ahnnieanezi@gmail.com>
2024-03-09 10:54:39 +0100

    style position for career content

--- src/Components/Careeres.jsx
+++ src/Components/Careeres.jsx
@@ -2,17 +2,20 @@ import React from "react"
 export default function Careers() {
     return(
         <>
-            <div>
+            <div className=" relative flex ml-40 my-32">
                 <div>
-                    <img src="./src/assets/imagecarrer.png" alt="person with wearing VR machine" />
+                    <img src="./src/assets/images/desktop/image-interactive.jpg" width={500} alt="person with wearing VR machine" />
                 </div>
-                <div>
-                    <h2>THE HEADER IN INTERACTIVE VR</h2>
-                    <p>Creates highly immersive environments that surround users, 
-                        making them feel as though they are physically present within
-                         the digital space. This immersive experience is achieved through
-                          the use of VR headsets, which typically cover the user's field of 
-                          view and block out the real-world environment.</p>
+                <div className="inline-block w-[500px] p-10 absolute right-52 bg-white top-32 ">
+                    <div className="w-80">
+                        <h2 className="pb-2 text-3xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">THE HEADER IN INTERACTIVE VR</h2>
+                        <p className="text-sm">Creates highly immersive environments that surround users, 
+                            making them feel as though they are physically present within
+                            the digital space. This immersive experience is achieved through
+                            the use of VR headsets.
+                        </p>
+                    </div>
+                    
                 </div>
             </div>
         </>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar"
 export default function Home() {
     return(
         <>
-            <div className="bg-hero-pattern bg-no-repeat bg-cover h-96 text-white ">
+            <div className="bg-hero-pattern bg-no-repeat bg-cover bg-center h-96 text-white ">
                 <Navbar />
                 <h1 className="text-5xl font-light inline-block w-96 m-10 border border-white p-5">IMMERSIVE EXPERIENCES THAT DELIVER</h1>
             </div>
