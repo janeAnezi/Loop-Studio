67e6dccdf37425a7f842de6428837288a97d02f2
Anezijane <ahnnieanezi@gmail.com>
2024-03-10 03:06:47 +0100

    update nav list items to links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,13 +2,13 @@ export default function Navbar() {
     return(
         <>
             <nav className="flex justify-between p-10">
-                <h4>LOOPSTUDIOS</h4>
+                <h4><a href="#">LOOPSTUDIOS</a></h4>
                 <ul className="flex space-x-3">
-                    <li>About</li>
-                    <li>Careers</li>
-                    <li>Events</li>
-                    <li>Products</li>
-                    <li>Support</li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Support</a></li>
                 </ul>
             </nav>
         </>
