ee2a471e7fa5c3d7c84b69887bb3c08fca57c808
Anezijane <ahnnieanezi@gmail.com>
2024-03-12 01:48:35 +0100

    add times icon and set to hidden

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,25 @@
-import { FaAlignJustify } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
     return(
         <>
             <nav className="flex justify-between p-10 w-full">
                 <h4><a href="#">LOOPSTUDIOS</a></h4>
-                <FaAlignJustify size={32}/>
-                <ul className="flex space-x-3 max-[900px]:hidden">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Careers</a></li>
-                    <li><a href="#">Events</a></li>
-                    <li><a href="#">Products</a></li>
-                    <li><a href="#">Support</a></li>
-                </ul>
+                <div>
+                   <div id="toggle">
+                        <FaBars size={32}/> 
+                        <FaTimes size={32} className="hidden"/>
+                    </div>
+                    <ul className="flex space-x-3 max-[600px]:hidden ">
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Support</a></li>
+                    </ul>
+                </div>
+                
             </nav>
         </>
     )
