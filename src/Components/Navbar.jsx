44454dd1c57c145a0ae97fcedba42d30f5965464
Anezijane <ahnnieanezi@gmail.com>
2024-03-12 01:28:22 +0100

    set navlinks to hidden on small screen

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
         <>
             <nav className="flex justify-between p-10">
                 <h4><a href="#">LOOPSTUDIOS</a></h4>
-                <ul className="flex space-x-3">
+                <ul className="flex space-x-3 max-[900px]:hidden">
                     <li><a href="#">About</a></li>
                     <li><a href="#">Careers</a></li>
                     <li><a href="#">Events</a></li>
