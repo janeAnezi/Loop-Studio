9c8cdf89db941822373b6a1e72bbff1784b2ec7f
Anezijane <ahnnieanezi@gmail.com>
2024-03-12 01:35:10 +0100

    add handbuger icon

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,11 @@
+import { FaAlignJustify } from "react-icons/fa";
+
 export default function Navbar() {
     return(
         <>
-            <nav className="flex justify-between p-10">
+            <nav className="flex justify-between p-10 w-full">
                 <h4><a href="#">LOOPSTUDIOS</a></h4>
+                <FaAlignJustify size={32}/>
                 <ul className="flex space-x-3 max-[900px]:hidden">
                     <li><a href="#">About</a></li>
                     <li><a href="#">Careers</a></li>
